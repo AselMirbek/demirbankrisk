997e97d9623d8292f8eb062a25b9957586557bb4
Asel <109470938+AselMirbek@users.noreply.github.com>
2025-11-24 11:20:27 +0600

    Refactor admin.tsx to include Header and CountryLimits

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,5 +1,13 @@
-{
-  "rewrites": [
-    { "source": "/(.*)", "destination": "/" }
-  ]
-}
+import Header from "@/components/Header";
+import CountryLimits from "@/components/AdminCountryLimits";
+
+const Admin = () => {
+  return (
+    <>
+      <Header />
+      <AdminCountryLimits />
+    </>
+  );
+};
+
+export default Admin;
