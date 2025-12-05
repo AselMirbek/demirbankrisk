e5e27f9dfbe15e13fea3095d2df3c5a26d6c076f
Asel <109470938+AselMirbek@users.noreply.github.com>
2025-12-05 11:43:41 +0600

    Create CUST368.tsx

---
+++ src/pages/CUST368.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header";
+import CustomerBiometricException  from "@/components/CustomerBiometricException";
+
+const CustomerBiometricException = () => {
+  return (
+    <>
+      <Header />
+      <CustomerBiometricException />
+    </>
+  );
+};
+
+export default CustomerBiometricException;
